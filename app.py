4b1a002be5914197a4c44b125f84e07ae974051e
paniww <fannieeimup21@gmail.com>
2026-06-11 15:28:59 +0700

    App.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-
 import streamlit as st
 import joblib
 
 model = joblib.load("model_naive_bayes.pkl")
 tfidf = joblib.load("model_tfidf.pkl")
 
-st.title("Klasifikasi Caption TikTok Shop")
+st.title("Sistem Klasifikasi Indikasi Akun Buzzer pada TikTok Shop")
 
-caption = st.text_area("Masukkan Caption", height=200)
+st.write(
+    "Sistem ini mengidentifikasi indikasi akun buzzer berdasarkan caption promosi produk yang dipublikasikan pada TikTok Shop menggunakan metode TF-IDF dan algoritma Naïve Bayes."
+)
+
+caption = st.text_area(
+    "Masukkan Caption",
+    height=200
+)
 
 if st.button("Analisis"):
 
@@ -15,7 +21,16 @@ if st.button("Analisis"):
 
     hasil = model.predict(data)[0]
 
+    prob = model.predict_proba(data)[0]
+
+    st.subheader("Hasil Klasifikasi")
+
     if hasil == 1:
-        st.success("BUZZER")
+        st.error("BUZZER")
     else:
         st.success("NON-BUZZER")
+
+    st.write("---")
+
+    st.write(f"Probabilitas Label 0 : {prob[0]*100:.2f}%")
+    st.write(f"Probabilitas Label 1 : {prob[1]*100:.2f}%")
